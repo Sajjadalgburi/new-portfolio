8f8149da95d76373823afafd6ad2600e3bdf134a
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-06-18 03:04:35 -0600

    fix: change default theme from dark to light in ThemeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
           "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
           fontSans.variable,
         )}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           <TooltipProvider delayDuration={0}>
             {children}
             <Navbar />
